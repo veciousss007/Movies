21b2086673492125d77f328f4cd6a61d930448e8
Asadbek <vecious007@gmail.com>
2026-01-03 05:52:10 +0500

    Initial Commit

---
+++ Movies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Asadbek Mamutov on 03/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Asadbek Mamutov on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
